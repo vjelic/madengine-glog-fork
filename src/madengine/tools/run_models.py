7a68392b07968f6f96ca10de9c62c6a0b525ce84
Gene Su <Gene.Su@amd.com>
2025-06-25 11:42:22 -0700

    log images
    Signed-off-by: Gene Su <Gene.Su@amd.com>

--- src/madengine/tools/run_models.py
+++ src/madengine/tools/run_models.py
@@ -617,6 +617,8 @@ class RunModels:
                 )
             print(f"BASE DOCKER is {run_details.base_docker}")
 
+            self.console.sh("docker images")
+
             # print base docker image digest
             run_details.docker_sha = self.console.sh("docker inspect --format='{{index .RepoDigests 0}}' " + run_details.base_docker + " | cut -d '@' -f 2")
             print(f"BASE DOCKER SHA is {run_details.docker_sha}")
