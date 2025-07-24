d5b9cf8c0a0e9a987f2302d73034d53eafbc1e0e
Stephen Shao <yu.shao@amd.com>
2025-07-23 23:10:08 -0400

    Added share memory control and disable ipc option when shm-size is set (#22)

--- src/madengine/tools/run_models.py
+++ src/madengine/tools/run_models.py
@@ -694,6 +694,10 @@ class RunModels:
         docker_options += self.get_mount_arg(mount_datapaths)
         docker_options += f" {run_details.additional_docker_run_options}"
 
+        # if --shm-size is set, remove --ipc=host
+        if "SHM_SIZE" in self.context.ctx:
+            docker_options = docker_options.replace("--ipc=host", "")
+
         print(docker_options)
 
         # get machine name
@@ -908,6 +912,13 @@ class RunModels:
         # Taking gpu arch from context assumes the host image and container have the same gpu arch.
         # Environment variable updates for MAD Public CI
         run_details.gpu_architecture = self.context.ctx["docker_env_vars"]["MAD_SYSTEM_GPU_ARCHITECTURE"]
+
+        # Check the setting of shared memory size
+        if "SHM_SIZE" in self.context.ctx:
+            shm_size = self.context.ctx["SHM_SIZE"]
+            if shm_size:
+                run_details.additional_docker_run_options += f" --shm-size={shm_size}"
+                print(f"Using SHM_SIZE from context: {shm_size}")
 
         # Check if model is deprecated
         if model_info.get("is_deprecated", False):
